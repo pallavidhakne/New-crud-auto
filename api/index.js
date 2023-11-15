218de7b99bfd0206b08e37646186f043aabfe413
pallavidhakne <pmdhakne2002@gmail.com>
2023-11-15 13:57:27 +0530

    mongodb connected

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
 
+mongoose
+  .connect("mongodb+srv://pallavi:pallavi@crudauth.cqigqcr.mongodb.net/")
+ 
+ .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app =express();
 app.listen(3000,()=>{
     console.log('Server listening on port 3000');
